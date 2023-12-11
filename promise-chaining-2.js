8d01693200a9a49325dc6b37967769876c4105d5
Buse Duran <buseduran500@gmail.com>
2023-12-11 18:48:43 +0300

    feat: async

--- promise-chaining-2.js
+++ promise-chaining-2.js
@@ -1,11 +1,31 @@
 require('./src/db/mongoose')
 const User = require('./src/models/user')
+const Task = require('./src/models/task')
 
-User.findByIdAndUpdate('656ce0a3999ccaab20d0034a', { age: 22 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 22 })
-}).then((result) => {
-    console.log(result)
+
+// User.findByIdAndUpdate('656ce0a3999ccaab20d0034a', { age: 22 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 22 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+//create deleteTaskandCount as an async function
+// - accept id of task to remove
+//use await to delete task and count up incomplete task
+//return the count
+//test your work
+
+const deleteTaskandCount = async (id) => {
+    const remove = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ status: false })
+    return count
+}
+
+deleteTaskandCount('656de6bbcb114a2e7d0e7f51').then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
--- src/index.js
+++ src/index.js
@@ -8,52 +8,64 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
     console.log(req.body)
 
-    user.save()
-        .then(() => {
-            res.send(user)
-        })
-        .catch((error) => {
-            res.status(201).send(error)
-        })
+    try {
+        await user.save()
+        res.status(201).send(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
 })
-app.get('/users', (req, res) => { // get all users
-    User.find({})
-        .then((users) => {
-            res.send(users)
-        })
-        .finally((error) => {
-            res.status(500).send(error)
-        })
 
+app.get('/users', async (req, res) => { // get all users
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
 })
-app.get('/users/:id', (req, res) => { //get user by id
+
+app.get('/users/:id', async (req, res) => { //get user by id
     //id parametrelerden alinir
     const __id = req.params.id
 
-    User.findById(__id)
-        .then((user) => {
-            res.send(user)
-        })
-        .catch((error) => {
-            res.status(404).send(error)
-        })
+    try {
+        const user = await user.findById(__id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(404).send(error)
+    }
 })
 
-app.post('/tasks', (req, res) => { //create task
+app.post('/tasks', async (req, res) => { //create task
     const task = new Task(req.body)
     console.log(req.body)
 
-    task.save()
-        .then(() => {
-            res.send(task)
-        })
-        .catch((error) => {
-            res.status(201).send(error)
-        })
+    try {
+        await task.save()
+        res.send(task)
+    }
+    catch (error) {
+        res.status(201).send(error)
+    }
+
+    // task.save()
+    //     .then(() => {
+    //         res.send(task)
+    //     })
+    //     .catch((error) => {
+    //         res.status(201).send(error)
+    //     })
 })
 
 app.get('/tasks', (req, res) => {
