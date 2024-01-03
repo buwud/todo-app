2858826574cc010f8ffade0ecb69f14f564ab2de
Buse Duran <buseduran500@gmail.com>
2024-01-03 22:27:57 +0300

    feat: basic hashing algorithm

--- src/index.js
+++ src/index.js
@@ -16,3 +16,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+//basic hashing algorithm
+const bcrypt = require('bcryptjs')
+const myFunc = async () => {
+    const password = 'B123.'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('b123.', hashedPassword)
+    console.log(isMatch)
+}
+myFunc()
