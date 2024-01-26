2067e8b0f35014b3db5e31ec8c9a98081ac72ebd
Buse Duran <buseduran500@gmail.com>
2024-01-26 23:21:58 +0300

    feat: add user login functionality

--- src/models/user.js
+++ src/models/user.js
@@ -41,14 +41,23 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+//user login validate
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user)
+        throw new Error('Unable to login!')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch)
+        throw new Error('Unable to login!')
+}
 //pre-before an event, before users are saved
 //next-it says the process is done
 userSchema.pre('save', async function (next) {
     const user = this
-
-    //console.log('just before saving!')
     //hash the password
-
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
